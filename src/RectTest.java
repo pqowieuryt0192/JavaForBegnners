6599c97b2ff200ac887162a24ef72908055c4990
liziliuzhao80 <liziliuzhao80@gmail.com>
2025-05-26 11:11:41 +0900

    四角形の色を変えてみよう

--- src/RectTest.java
+++ src/RectTest.java
@@ -2,8 +2,11 @@
 public class RectTest extends MyFrame{
 	public void run() 
 	{
-		fillRect(30,50,200,100);
+		setColor(0,255,255);
+		fillRect(30,40,50,100);
+		setColor(0,77,153);
 		fillRect(130,40,50,100);
+		setColor(26,153,0);
 		fillRect(30,200,150,10);
 	}
 
