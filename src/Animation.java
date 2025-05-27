10f62c5bb789cd0f12ffc7193ec5a62ef185190a
liziliuzhao80 <liziliuzhao80@gmail.com>
2025-05-27 11:54:49 +0900

    練習問題5-2

--- src/Animation.java
+++ src/Animation.java
@@ -1,32 +1,41 @@
 
 public class Animation extends MyFrame{
 	public void run() {
-int y=200;
+int y=100;
 int x=30;
 
 
 
-while(y>=80) 
+while(y<=200) 
+{
+clear();
+setColor(0,128,0);
+fillOval(x,y,100,100);
+x+=5;
+y+=5;
+
+sleep(0.06);
+}
+while(x>=30) 
 {
 clear();
 setColor(0,128,0);
 fillOval(x,y,100,100);
 
-y-=5;
+x-=5;
 
 sleep(0.06);
+
 }
 while(x<=170) 
 {
 clear();
 setColor(0,128,0);
 fillOval(x,y,100,100);
-
 x+=5;
+y-=5;
 
 sleep(0.06);
-
 }
-
 }
 }
