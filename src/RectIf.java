ab98e9150e36538fcf0a8164701f2e854a0a4aae
liziliuzhao80 <liziliuzhao80@gmail.com>
2025-05-27 10:14:48 +0900

    EX　だんだん色濃く

--- src/RectIf.java
+++ src/RectIf.java
@@ -4,7 +4,7 @@ public void run() {
 	int x=30;
 	setColor(0,128,0);
 	int i;
-	for(i=0 ; i<9 ; i++)
+	for(i=0 ; i<19 ; i++)
 	{
 		if (i==0)
 		{
@@ -53,10 +53,61 @@ public void run() {
 			setColor(204,204,204);
 			fillRect(x,80,10,100);
 		}
-		else 
+		else if(i==9)
 		{
 			setColor(230,230,230);
 			fillRect(x,80,10,100);
+		}
+		else if(i==10)
+		{
+			setColor(230,230,230);
+			fillRect(x,80,10,100);
+		}
+		else if(i==11)
+		{
+			setColor(204,204,204);
+			fillRect(x,80,10,100);
+		}
+		else if(i==12)
+		{
+			setColor(179,179,179);
+			fillRect(x,80,10,100);
+		}
+		else if(i==13)
+		{
+			setColor(153,153,153);
+			fillRect(x,80,10,100);
+		}
+		else if(i==14)
+		{
+			setColor(128,128,128);
+			fillRect(x,80,10,100);
+		}
+		else if(i==15)
+		{
+			setColor(102,102,102);
+			fillRect(x,80,10,100);
+		}
+		else if(i==16)
+		{
+			setColor(77,77,77);
+			fillRect(x,80,10,100);
+		}
+		else if(i==17)
+		{
+			setColor(51,51,51);
+			fillRect(x,80,10,100);
+		}
+		else if(i==18) 
+		{
+			setColor(26,26,26);
+			fillRect(x,80,10,100);
+		}
+		else
+		{
+			setColor(0,0,0);
+			fillRect(x,80,10,100);
+			
 		}
 		x+=20;
 	}
