aaf3e71bed3ded0f2605644be1396d088d4d1df9
liziliuzhao80 <liziliuzhao80@gmail.com>
2025-05-27 09:50:54 +0900

    練習問題4-2

--- src/RectIf.java
+++ src/RectIf.java
@@ -8,45 +8,45 @@ public void run() {
 	{
 		if (i==0)
 		{
-			fillRect(x,80,10,100);
+			fillRect(x,80,10,10);
 			
 		}
 		else if(i==1) 
 		{
-			fillRect(x,85,10,100);
+			fillRect(x,80,10,20);
 		}
 		
 		else if(i==2)
 		{
-			fillRect(x,90,10,100);
+			fillRect(x,80,10,30);
 		}
 		else if(i==3)
 		{
-			fillRect(x,95,10,100);
+			fillRect(x,80,10,40);
 		}
 		else if(i==4)
 		{
-			fillRect(x,100,10,100);
+			fillRect(x,80,10,50);
 		}
 		else if(i==5)
 		{
-			fillRect(x,105,10,100);
+			fillRect(x,80,10,60);
 		}
 		else if(i==6)
 		{
-			fillRect(x,110,10,100);
+			fillRect(x,80,10,70);
 		}
 		else if(i==7)
 		{
-			fillRect(x,115,10,100);
+			fillRect(x,80,10,80);
 		}
 		else if(i==8)
 		{
-			fillRect(x,120,10,100);
+			fillRect(x,80,10,90);
 		}
 		else 
 		{
-			fillRect(x,125,10,100);
+			fillRect(x,80,10,100);
 		}
 		x+=20;
 	}
