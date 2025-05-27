8b63b572f7f7e86d6bf6831ab36b2b4ae1ef8122
liziliuzhao80 <liziliuzhao80@gmail.com>
2025-05-27 10:04:06 +0900

    練習問題4-3

--- src/RectIf.java
+++ src/RectIf.java
@@ -8,44 +8,54 @@ public void run() {
 	{
 		if (i==0)
 		{
-			fillRect(x,80,10,10);
+			setColor(0,0,0);
+			fillRect(x,80,10,100);
 			
 		}
 		else if(i==1) 
 		{
-			fillRect(x,80,10,20);
+			setColor(26,26,26);
+			fillRect(x,80,10,100);
 		}
 		
 		else if(i==2)
 		{
-			fillRect(x,80,10,30);
+			setColor(51,51,51);
+			fillRect(x,80,10,100);
 		}
 		else if(i==3)
 		{
-			fillRect(x,80,10,40);
+			setColor(77,77,77);
+			fillRect(x,80,10,100);
 		}
 		else if(i==4)
 		{
-			fillRect(x,80,10,50);
+			setColor(102,102,102);
+			fillRect(x,80,10,100);
 		}
 		else if(i==5)
 		{
-			fillRect(x,80,10,60);
+			setColor(128,128,128);
+			fillRect(x,80,10,100);
 		}
 		else if(i==6)
 		{
-			fillRect(x,80,10,70);
+			setColor(153,153,153);
+			fillRect(x,80,10,100);
 		}
 		else if(i==7)
 		{
-			fillRect(x,80,10,80);
+			setColor(179,179,179);
+			fillRect(x,80,10,100);
 		}
 		else if(i==8)
 		{
-			fillRect(x,80,10,90);
+			setColor(204,204,204);
+			fillRect(x,80,10,100);
 		}
 		else 
 		{
+			setColor(230,230,230);
 			fillRect(x,80,10,100);
 		}
 		x+=20;
